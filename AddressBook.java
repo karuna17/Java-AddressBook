5395065234ddc8270ba2a1cce686e6b2aea9c4a2
karuna17 <karunakakade8@gmail.com>
2021-05-30 19:57:34 +0530

    Added: Add multiple Person's Details in address book

---
+++ AddressBook.java
@@ -0,0 +1,94 @@
+
+public class AddressBook {
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String city;
+	private String state;
+	private String pin;
+	private String mobileNo;
+	private String email;
+	
+	public AddressBook(int id, String firstName, String lastName, String city, String state, String pin,
+			String mobileNo, String email) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.city = city;
+		this.state = state;
+		this.pin = pin;
+		this.mobileNo = mobileNo;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getPin() {
+		return pin;
+	}
+
+	public void setPin(String pin) {
+		this.pin = pin;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "AddressBook [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", city=" + city
+				+ ", state=" + state + ", pin=" + pin + ", mobileNo=" + mobileNo + ", email=" + email + "]";
+	}	
+}
